bd6a6eac405e20efde2429a492484da17a14554a
FauzanAr <faurinta12345@gmail.com>
2019-10-16 00:07:29 +0700

    Creating model, controlle, also resource for catalogtype

--- admin/app/Http/Resources/CatalogTypeResource.php
+++ admin/app/Http/Resources/CatalogTypeResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductResource extends JsonResource
+class CatalogTypeResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,12 +14,6 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id'=>$this->id,
-            'catalogName'=>$this->catalogName,
-            'catalogType'=>$this->catalogType,
-            'picture'=>'http://i.pravatar.cc',
-            'harga'=>$this->harga,
-        ];
+        return parent::toArray($request);
     }
 }
