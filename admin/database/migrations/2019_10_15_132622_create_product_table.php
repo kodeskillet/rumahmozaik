6e5b0e2e2cde8dd03c3eec14c9b906f6b18b94b8
FauzanAr <faurinta12345@gmail.com>
2019-10-15 20:27:30 +0700

    fixing product migration

--- admin/database/migrations/2019_10_15_132622_create_product_table.php
+++ admin/database/migrations/2019_10_15_132622_create_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCatalogTable extends Migration
+class CreateProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCatalogTable extends Migration
      */
     public function up()
     {
-        Schema::create('catalog', function (Blueprint $table) {
+        Schema::create('product', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('catalogName');
             $table->integer('catalogType');
@@ -29,6 +29,6 @@ class CreateCatalogTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('catalog');
+        Schema::dropIfExists('product');
     }
 }
