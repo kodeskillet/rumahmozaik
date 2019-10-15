57a8d40ab6c6d710b09dff1800a9dda672e1f740
FauzanAr <faurinta12345@gmail.com>
2019-10-15 20:14:30 +0700

    Adding migration for catalog

---
+++ admin/database/migrations/2019_10_15_130808_create_catalog_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCatalogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catalog', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('catalogName');
+            $table->integer('catalogType');
+            $table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalog');
+    }
+}
