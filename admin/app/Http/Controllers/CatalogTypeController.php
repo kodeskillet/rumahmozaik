b122aad41f3ef76f9a92869afd6abf79d3341eb0
FauzanAr <faurinta12345@gmail.com>
2019-10-16 00:11:45 +0700

    Creating function to catalogtypecontroller

--- admin/app/Http/Controllers/CatalogTypeController.php
+++ admin/app/Http/Controllers/CatalogTypeController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\CatalogType;
 
 class CatalogTypeController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show ($id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
 }
