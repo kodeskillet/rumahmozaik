2fbb6d60a272550cbf3ff16eaadaf8f9c0e990ca
FauzanAr <faurinta12345@gmail.com>
2019-10-15 20:32:29 +0700

    Creating order item table

---
+++ admin/database/migrations/2019_10_15_132901_create_order_item_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_item', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('order_id');
+            $table->integer('product_id');
+            $table->integer('picture_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_item');
+    }
+}
