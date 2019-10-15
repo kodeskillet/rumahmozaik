4f1e39a89fa9ca55499a7abf95820855cb6abfd9
FauzanAr <faurinta12345@gmail.com>
2019-10-16 00:03:21 +0700

    modify product controller

--- admin/app/Http/Controllers/ProductController.php
+++ admin/app/Http/Controllers/ProductController.php
@@ -11,7 +11,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $Product = Product::paginate(30);
+        $Product = Product::all();
         return ProductResource::Collection($Product);
     }
 
