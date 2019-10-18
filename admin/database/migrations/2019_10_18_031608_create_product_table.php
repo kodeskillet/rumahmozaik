1377003e381dca8e50383d75a57bacabc4e8f9a4
FauzanAr <faurinta12345@gmail.com>
2019-10-18 10:16:56 +0700

    Re-create product table migration

---
+++ admin/database/migrations/2019_10_18_031608_create_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('productName');
+            $table->integer('catalogType');
+            $table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
