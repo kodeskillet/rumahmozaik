c79586a0d0441623980dc7eadf5b074f99624957
FauzanAr <faurinta12345@gmail.com>
2019-10-19 01:49:24 +0700

    Modify product pages

--- admin/resources/views/pages/product.blade.php
+++ admin/resources/views/pages/product.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 
-
+<div class="row">
+    <div class="col-md-11">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="title">{{ _('Edit Product') }}</h5>
+            </div>
+            <form method = "post" action="http://localhost:8000/api/product"></form>
+        </div>
+    </div>
+</div>
 
 @endsection
