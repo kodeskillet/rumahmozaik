e13358e5b1d2c38bbbab7f562e6ac2df94e10c77
FauzanAr <faurinta12345@gmail.com>
2019-10-18 21:43:16 +0700

    Removing any unused item at dashboard admin

--- admin/resources/views/layouts/navbars/sidebar.blade.php
+++ admin/resources/views/layouts/navbars/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="sidebar">
     <div class="sidebar-wrapper">
         <div class="logo">
-            <a href="#" class="simple-text logo-mini">{{ _('WD') }}</a>
-            <a href="#" class="simple-text logo-normal">{{ _('White Dashboard') }}</a>
+            <a href="#" class="simple-text logo-mini">{{ _('RM') }}</a>
+            <a href="#" class="simple-text logo-normal">{{ _('Rumah Mozaik') }}</a>
         </div>
         <ul class="nav">
             <li @if ($pageSlug == 'dashboard') class="active " @endif>
