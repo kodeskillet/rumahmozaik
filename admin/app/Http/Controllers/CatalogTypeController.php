dc8b9cbe31d86cdf5a854ab9c84c532a7dd3bc2b
FauzanAr <faurinta12345@gmail.com>
2019-10-18 10:32:55 +0700

    Adding 2 of catalogtype and product controller

---
+++ admin/app/Http/Controllers/CatalogTypeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\CatalogType;
+use App\Http\Resources\CatalogTypeResource;
+
+class CatalogTypeController extends Controller
+{
+    public function index()
+    {
+        $type = CatalogType::all();
+        return CatalogTypeResource::Collection($type);
+    }
+
+    public function store(Request $request)
+    {
+        $type = $request->isMethod('put') ? CatalogType::findOrFail($request->id) : new CatalogType;
+        $type->name = $request->input('name');
+
+        if($type->save()){
+            return new CatalogTypeResource($type);
+        }
+    }
+
+    public function show ($id)
+    {
+        $type = CatalogType::findOrFail($id);
+        return new CatalogTypeResource($type);
+    }
+
+    public function destroy($id)
+    {
+        $type = CatalogTypeResource::findOrFail($id);
+
+        //  Delete the post, return as confirmation
+        if ($type->delete()) {
+            return new CatalogTypeResource($post);
+        }
+    }
+}
