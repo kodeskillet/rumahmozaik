a0133e504b2313fa82f622507767fc19de9b4071
AhmadSyifaur <ahmadsyifaur11@gmail.com>
2019-12-18 02:37:24 +0700

    create about us page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,19 +1,41 @@
 <template>
-  <section class="about">
-    <h1>This is an about page</h1>
+  <section class="about pb-12 pd-12">
+    <v-container>
+      <br>
+      <v-row align="center" justify="center">
+        <v-img src="/assets/images/char.png" max-width="300" max-height="300"></v-img>
+      </v-row>
+      <br>
+      <h2 align="center">About Us</h2>
+      <blockquote
+        class="blockquote"
+      >What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</blockquote>
+      <hr>
+      <br>
+      <br>
+      <v-row align="center" justify="center">
+        <v-avatar width="300" height="300">
+          <img src="/assets/images/avatar.jpg" height="100" alt="John">
+        </v-avatar>
+      </v-row>
+      <blockquote class="blockquote" align="center">
+        Name : Ngentod <br>
+        FB    : wwwwwwwwwwwww <br>
+        IG    : wlwlwlwlw <br>
+        BLOG  : jwhwfwhw <br>
+      </blockquote>
+    </v-container>
   </section>
 </template>
 
 <script>
-  // Ini ndausah diumek
+// Ini ndausah diumek
 export default {
-  name: 'About',
-  data: () => ({
-
-  })
-}
+  name: "About",
+  data: () => ({})
+};
 </script>
 
 <style>
-  /*styling disini*/
+/*styling disini*/
 </style>
