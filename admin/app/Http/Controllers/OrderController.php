cd6a837d6b1db82a999b131116d6cdf9b3b2d115
FauzanAr <faurinta12345@gmail.com>
2019-12-18 23:27:33 +0700

    Adding store function

--- admin/app/Http/Controllers/OrderController.php
+++ admin/app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Order;
 use App\Product;
 use App\CatalogType;
+use App\OrderItem;
 use App\Laravue\JsonResponse;
 use App\Http\Resources\OrderResource;
 use Illuminate\Support\Facades\Validator;
@@ -54,6 +55,40 @@ class OrderController extends Controller
         ->get();
         $json = json_encode(new OrderResource($order));
         return $json;
+    }
+
+    public function store(Request $request)
+    {
+        $order = new Order;
+        $order->name = $request->name;
+        $order->email = $request->email;
+        $order->whatsapp = $request->whatsapp;
+        $order->status = "PROSES";
+        $order->save();
+
+        $orderId = $order->id;
+        $cart = $request->cart;
+        if(is_array($cart)){
+            foreach($cart as $item){
+                $order_item = new OrderItem;
+                $order_item->order_id = $orderId;
+                $order_item->product_id = $item->productId;
+                $order_item->amount = $item->amount;
+                $order_item->save();
+            }
+            return response()->json([
+                'message' => 'Success'
+            ]);
+        }
+        $order_item = new OrderItem;
+        $order_item->order_id = $orderId;
+        $order_item->product_id = $cart->productId;
+        $order_item->amount = $cart->amount;
+        $order_item->save();
+
+        return response()->json([
+            'message' => 'Success'
+        ]);
     }
 
     public function statechange(Request $request)
