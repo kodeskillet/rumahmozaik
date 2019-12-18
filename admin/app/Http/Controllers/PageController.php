0fa723e2d4d4504ec5bb8264e481ffe304dc9d00
FauzanAr <faurinta12345@gmail.com>
2019-12-18 20:33:26 +0700

    creating order page

--- admin/app/Http/Controllers/PageController.php
+++ admin/app/Http/Controllers/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\CatalogTypeController;
 use App\Http\Controllers\Product;
+use App\Http\Controllers\Order;
 
 class PageController extends Controller
 {
@@ -70,5 +71,11 @@ class PageController extends Controller
     {
         $catalogs = $catalogs->destroy($id);
         return redirect()->action('PageController@catalog');
+    }
+
+    public function order(OrderController $orders)
+    {
+        $orders = $orders->index();
+        return view('pages.order', compact('orders'));
     }
 }
---
+++ admin/resources/views/pages/order.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app', ['page' => __('Order'), 'pageSlug' => 'order'])
+
+@section('content')
+
+@endsection
