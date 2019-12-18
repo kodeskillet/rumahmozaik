250874ab2d3ef94ce29484df781e3b8d21e3a9e7
FauzanAr <faurinta12345@gmail.com>
2019-12-18 19:15:11 +0700

    Adding status into migration

--- admin/app/Http/Controllers/OrderController.php
+++ admin/app/Http/Controllers/OrderController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
+use App\Product;
+use App\CatalogType;
+use App\Laravue\JsonResponse;
+use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Resources\CatalogTypeResource;
+use Illuminate\Support\Facades\DB;
+
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        $orders = Order::all();
+        foreach($orders as $order){
+            $order->orderItem = DB::table('order_item')
+                                ->select('product_id', 'amount')
+                                ->where('order_id',$order->id)
+                                ->get();
+        }
+        $json = json_encode(OrderResource::collection($orders));
+        return $json;
+    }
 }
--- admin/app/Http/Resources/OrderResource.php
+++ admin/app/Http/Resources/OrderResource.php
@@ -14,6 +14,10 @@ class OrderResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'whatsapp' => $this->whatsapp
+        ];
     }
 }
